f8a8b0473217f5a2fd6e88cb52b5b0cc221999df
Leonardo Rotondano <leorotondano@gmail.com>
2022-01-26 15:54:55 -0300

    Adicionando versão com ajustes nas configurações do swagger

--- drivers/webserver/swagger/swagger.js
+++ drivers/webserver/swagger/swagger.js
@@ -1,9 +1,14 @@
 const swaggerAutogen = require('swagger-autogen')()
-
 const outputFile = process.cwd()+'/drivers/webserver/swagger/swagger_output.json'
 const endpointsFiles = [process.cwd()+'/drivers/webserver/routes/index.js']
+const config = require('../../../config')
 
-swaggerAutogen(outputFile, endpointsFiles).then(()=> {
+
+const doc = {
+    host: "localhost:"+config.PORT
+}
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(()=> {
     require('../server')
 });
 
